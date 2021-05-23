b1d7dc182d14b389e9c22b09a646b2c34d91f5ae
wangsichen23 <ppxwsc@gmail.com>
2021-05-23 21:59:59 +0800

    Add files via upload

---
+++ 3_Queue/ex1_Queueing.c
@@ -0,0 +1,144 @@
+#include<stdio.h>
+#include<string.h>
+#include<malloc.h>
+
+#define OK  1
+#define ERROR  0
+#define TRUE 1
+#define FALSE 0
+
+#define QueueElementType int
+#define MAXSIZE 50
+
+typedef struct Node
+{
+	QueueElementType data;     /*数据域*/
+	struct Node *next;     /*指针域*/
+}LinkQueueNode;
+
+typedef struct
+{
+	LinkQueueNode *front;
+	LinkQueueNode *rear;
+}LinkQueue;
+
+/*初始化操作。*/
+int InitQueue(LinkQueue *Q)
+{
+	/* 将Q初始化为一个空的链队列 */
+	Q->front=(LinkQueueNode *)malloc(sizeof(LinkQueueNode));
+	if(Q->front!=NULL)
+	{
+		Q->rear=Q->front;
+		Q->front->next=NULL;
+ 		return(TRUE);
+	}
+	else return(FALSE);    /* 溢出！*/
+}
+
+/*判断队列是否为空*/
+int IsEmpty(LinkQueue *Q)
+{
+    if(Q->front==Q->rear)
+        return(TRUE);
+    else
+        return(FALSE);
+}
+
+/*入队操作。*/
+int EnterQueue(LinkQueue *Q, QueueElementType x)
+{
+	/* 将数据元素x,y插入到队列Q中 */
+	LinkQueueNode *NewNode;
+	NewNode=(LinkQueueNode * )malloc(sizeof(LinkQueueNode));
+	if(NewNode!=NULL)
+	{
+		NewNode->data=x;
+		NewNode->next=NULL;
+		Q->rear->next=NewNode;
+		Q->rear=NewNode;
+		return(TRUE);
+	}
+	else  return(FALSE);    /* 溢出！*/
+}
+
+/*出队操作。*/
+int DeleteQueue(LinkQueue *Q)
+{
+	/* 将队列Q的队头元素出队，并存放到x所指的存储空间中 */
+	LinkQueueNode * p;
+	int x;
+	if(Q->front==Q->rear)
+		return(FALSE);
+	p=Q->front->next;
+	Q->front->next=p->next;  /* 队头元素p出队 */
+	if(Q->rear==p)  /* 如果队中只有一个元素p，则p出队后成为空队 */
+		Q->rear=Q->front;
+	x=p->data;
+	free(p);   /* 释放存储空间 */
+	return(x);
+}
+
+float Queueing(QueueElementType m, QueueElementType total)
+{
+    LinkQueue P;    //到达时间
+    InitQueue(&P);
+    LinkQueue Q;    //办理时间
+    InitQueue(&Q);
+    int R[total];   //结束时间
+
+    int i,k,n,x,y,min,temp;
+    float t=0;
+
+    for(i=0;i<total;i++)
+    {
+        scanf("%d %d",&x,&y);
+        EnterQueue(&P,x);
+        EnterQueue(&Q,y);
+        R[i]=0;
+    }
+
+    R[0]=DeleteQueue(&P)+DeleteQueue(&Q);
+
+    while(!IsEmpty(&P))
+    {
+        k=0;n=0;
+        temp=DeleteQueue(&P);
+        min=R[0];
+        for(i=0;i<m;i++)
+            if(R[i]<min)
+            {
+                min=R[i];
+                n=i;
+            }
+        for(i=0;i<m;i++)
+            if(R[i]<=temp)
+            {
+                R[n]=temp+DeleteQueue(&Q);
+                k=1;
+                break;
+            }
+        if(k==1) continue;
+        t+=min-temp;
+        for(i=0;i<m;i++)
+            if(R[i]==min)
+            {
+                R[i]+=DeleteQueue(&Q);
+                break;
+            }
+    }
+    return t;
+}
+
+int main()
+{
+    int m,total;
+    float time;
+    while(scanf("%d",&m)!=EOF)
+    {
+        scanf("%d",&total);
+        time=Queueing(m,total);
+        printf("%.2f\n",time/total);
+    }
+    return 0;
+}
---
+++ 3_Queue/hw1_YangHuiTriangle.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+#define OK  1
+#define ERROR  0
+#define TRUE 1
+#define FALSE 0
+
+#define MAXSIZE 50
+
+typedef struct
+{
+	int element[MAXSIZE];  /* 队列的元素空间*/
+	int front;  /*头指针指示器*/
+	int rear;  /*尾指针指示器*/
+}SeqQueue;
+
+void InitQueue(SeqQueue *Q)
+{
+	Q->front=Q->rear=0;
+}
+
+int IsEmpty(SeqQueue *Q)
+{
+    if(Q->front==Q->rear)
+        return(TRUE);
+    else
+        return(FALSE);
+}
+
+int EnterQueue(SeqQueue *Q, int x)
+{
+	if((Q->rear+1)%MAXSIZE==Q->front)  /*队列已经满了*/
+		return(FALSE);
+	Q->element[Q->rear]=x;
+	Q->rear=(Q->rear+1)%MAXSIZE;  /* 重新设置队尾指针 */
+	return(TRUE);  /*操作成功*/
+}
+
+int DeleteQueue(SeqQueue *Q, int *x)
+{
+	if(Q->front==Q->rear)  /*队列为空*/
+		return(FALSE);
+	*x=Q->element[Q->front];
+	Q->front=(Q->front+1)%MAXSIZE;  /*重新设置队头指针*/
+	return(TRUE);  /*操作成功*/
+}
+
+int GetHead(SeqQueue *Q, int *x)
+{
+	if(Q->front==Q->rear)  /*队列为空*/
+		return(FALSE);
+	*x=Q->element[Q->front];
+	return(TRUE);  /*操作成功*/
+}
+
+void YangHuiTriangle(int n)
+{
+	int i,j,temp=0,x=0;
+	SeqQueue Q;
+	InitQueue(&Q);
+	EnterQueue(&Q,1);  /* 第一行元素入队*/
+	for(i=2;i<=n;i++)   /* 产生第n行元素并入队，同时打印第n-1行的元素*/
+	{
+		EnterQueue(&Q,1);   /* 第n行的第一个元素入队*/
+		for(j=1;j<=i-2;j++)  /* 利用队中第n-1行元素产生第n行的中间n-2个元素并入队*/
+		{
+			DeleteQueue(&Q,&temp);
+			printf("%d ",temp);     /* 打印第n-1行的元素*/
+			GetHead(&Q,&x);
+			temp=temp+x;      /*利用队中第n-1行元素产生第n行元素*/
+			EnterQueue(&Q,temp);
+		}
+		DeleteQueue(&Q,&x);
+		printf("%d ",x);    /* 打印第n-1行的最后一个元素*/
+		EnterQueue(&Q,1);   /* 第n行的最后一个元素入队*/
+		printf("\n");
+	}
+	while(!IsEmpty(&Q))
+    {
+        DeleteQueue(&Q,&x);
+        printf("%d ",x);
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    YangHuiTriangle(n);
+    return 0;
+}
