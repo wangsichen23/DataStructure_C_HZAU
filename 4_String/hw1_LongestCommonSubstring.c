8a5f6c4209731c0c22c6c1c9aed856b079c14846
wangsichen23 <ppxwsc@gmail.com>
2021-07-07 13:30:47 +0800

    Update hw1_LongestCommonSubstring.c

--- 4_String/hw1_LongestCommonSubstring.c
+++ 4_String/hw1_LongestCommonSubstring.c
@@ -1,3 +1,24 @@
+/*
+【问题描述】
+给定两个字符串，求出它们两个最长的相同子字符串的长度。
+最长公共子串(Longest Common Substring)是一个非常经典的面试题目，在实际的程序中也有很高的实用价值。同学们不应该单单只是写出该问题的基本解决代码而已，关键还是享受把算法一步步的优化，让时间和空间复杂度一步步的减少的惊喜。
+
+【输入形式】
+两个字符串
+
+【输出形式】
+最长的公共子串，若没有则输出“no”
+
+【样例输入】
+acbcbcef
+abcbced
+
+【样例输出】
+bcbce
+
+【温馨提示】
+问题的关键不在于提交代码并通过测试数据，而是你是否真正掌握了所有求解的算法。
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -34,7 +55,7 @@ void output(SString *s)
 }
 
 int SubString(SString *sub, SString s, int pos, int len)
-/*����s���±�pos��len���ַ����Ƶ�sub��*/
+/*将串s中下标pos起len个字符复制到sub中*/
 {
 	int i;
 	if (pos<0 || pos>s.len || len<1 || len>s.len-pos)
@@ -52,30 +73,30 @@ int SubString(SString *sub, SString s, int pos, int len)
 }
 
 int StrIndex(SString s,SString t)
-/*�������s���±�pos�𣬴�t��һ�γ��ֵ�λ�ã��ɹ�����λ����ţ����ɹ�����-1*/
+/*求从主串s的下标pos起，串t第一次出现的位置，成功返回位置序号，不成功返回-1*/
 {
 	int i, j, start;
 	if (t.len==0)
-		return(0);   /* ģʽ��Ϊ�մ�ʱ�������⴮��ƥ�䴮 */
+		return(0);   /* 模式串为空串时，是任意串的匹配串 */
 	start=0;
 	i=start;
-	j=0;  /* ������pos��ʼ��ģʽ����ͷ��0����ʼ */
+	j=0;  /* 主串从pos开始，模式串从头（0）开始 */
 	while (i<s.len && j<t.len)
 		if (s.ch[i]==t.ch[j])
 		{
 			i++;
 			j++;
-		}   /* ��ǰ��Ӧ�ַ����ʱ�ƽ� */
+		}   /* 当前对应字符相等时推进 */
 		else
 		{
-			start++;        /* ��ǰ��Ӧ�ַ�����ʱ���� */
+			start++;        /* 当前对应字符不等时回溯 */
 			i=start;
-			j=0;   /* ������start+1��ʼ��ģʽ����ͷ��0����ʼ*/
+			j=0;   /* 主串从start+1开始，模式串从头（0）开始*/
 		}
 		if (j>=t.len)
-			return(start);    /* ƥ��ɹ�ʱ������ƥ����ʼλ�� */
+			return(start);    /* 匹配成功时，返回匹配起始位置 */
 		else
-			return(-1);    /* ƥ�䲻�ɹ�ʱ������-1 */
+			return(-1);    /* 匹配不成功时，返回-1 */
 }
 
 int LCS(SString s1,SString s2)
